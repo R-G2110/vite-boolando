3514f8990377279c50e46264ffd973cdea764a31
R-G2110 <argeldelacruz86@gmail.com>
2023-10-10 15:32:58 +0200

    [Add]: header (to be edited) on App.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,9 +10,97 @@ export default {
 </script>
 
 <template>
-	<header>HEADER</header>
+	<header>
+
+		<div class="fixed  ">
+		
+			<div class="container flex justify-sb">
+			
+				<nav>
+				
+					<ul class="flex">
+					
+						<li>
+							<a href="#">Donna</a>
+						</li>
+					
+						<li>
+							<a href="#">Uomo</a>
+						</li>
+					
+						<li>
+							<a href="#">Bambini</a>
+						</li>
+					
+					</ul>
+				
+				</nav>
+
+				<div class="logo">
+
+					<a href="">
+						<img src="../assets/img/boolean-logo.png" alt="boolean-logo">
+					</a>
+				
+				</div>
+			
+				<nav>
+				
+					<ul class="flex">
+					
+						<li>
+						
+							<a href="#">
+								<i class="fa-solid fa-user-astronaut"></i>
+							</a>
+						
+						</li>
+					
+						<li>
+						
+							<a href="#">
+								<i class="fa-regular fa-heart"></i>
+							</a>
+						
+						</li>
+					
+						<li>
+						
+							<a href="#">
+								<i class="fa-solid fa-bag-shopping"></i>
+							</a>
+						
+						</li>
+					
+					</ul>
+				
+				</nav>
+			
+			</div>
+		
+		</div>
+
+</header>
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+@use '../scss/partials/utilities' as *;
+@use '../scss/partials/general' as *;
+
+
+header .fixed{
+	width: 100%;
+  height: 70px;
+  background-color: #FF6900;
+	position: fixed;
+	z-index: 999;
+	.container {
+		align-items: center;
+		.logo {
+			width: 150px;
+		}
+	}
+}
 
 </style>
