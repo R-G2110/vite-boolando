0cf469f66fde9b106c23e0d4b08a2caebe849ccd
R-G2110 <argeldelacruz86@gmail.com>
2023-10-10 16:40:49 +0200

    [Add]: footer (to be edited) on App

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,9 +10,128 @@ export default {
 </script>
 
 <template>
-	<footer>FOOTER</footer>
+	<footer>
+		<div class="container flex justify-sb">
+
+			<div class="privacy">
+			
+				<h4>Boolando s.r.l</h4>
+				<nav>
+				
+					<ul class="menu flex">
+					
+						<li>
+							
+							<a href="@">Informazioni legali</a>
+						
+						</li>
+					
+						<li>
+							
+							<a href="@">Informativa sulla privacy</a>
+						
+						</li>
+					
+						<li>
+							
+							<a href="@">Diritto di recesso</a>
+						
+						</li>
+					
+					</ul>
+				
+				</nav>
+			
+			</div>
+
+			<div class="social">
+
+				<h5>Trovaci anche su</h5>
+				<nav>
+
+					<ul class="menu flex">
+
+						<li>
+
+							<a href="#">
+
+								<i class="fa-brands fa-square-twitter"></i>
+
+							</a>
+
+						</li>
+
+						<li>
+
+							<a href="#">
+
+								<i class="fa-brands fa-square-facebook"></i>
+
+							</a>
+
+						</li>
+
+						<li>
+
+							<a href="#">
+
+								<i class="fa-brands fa-square-instagram"></i>
+
+							</a>
+
+						</li>
+
+						<li>
+
+							<a href="#">
+
+								<i class="fa-brands fa-square-pinterest"></i>
+
+							</a>
+
+						</li>
+
+						<li>
+
+							<a href="#">
+
+								<i class="fa-brands fa-square-youtube"></i>
+
+							</a>
+
+						</li>
+
+					</ul>
+
+				</nav>
+
+			</div>
+
+		</div>
+	</footer>
 </template>
 
-<style>
+<style lang="scss" scoped>
+	@use '../scss/main';
 
+	h5,
+	h4 {
+		color: white;
+	}
+
+	a:hover {
+		color: #FF6900;
+	}
+
+	footer{
+	  margin-top: 50px;
+	  padding: 20px;
+	  background-color: #1A1A1A;
+		.privacy a {
+		  font-size: 10px;
+		}
+		.social ul li{
+		  list-style: none;
+		}
+	}
 </style>
